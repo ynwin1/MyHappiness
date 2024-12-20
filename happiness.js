6b5e193fba1d531584f52abf548cd00420795d90
ynwin1 <ynwinubc24@gmail.com>
2024-12-20 12:50:59 -0800

    able to view prev years

--- happiness.js
+++ happiness.js
@@ -1,21 +1,109 @@
 document.addEventListener(
     "DOMContentLoaded",
     () => {
-        chrome.storage.local.get(null, function(items) {
+        let currentYear = new Date().getFullYear();
+
+        renderYear(currentYear);
+
+        const prevButton = document.getElementById("last-year");
+        prevButton.addEventListener("click", () => {
+            if (currentYear > new Date().getFullYear() - 2) {
+                currentYear--;
+                console.log("prev year: " + currentYear);
+                renderYear(currentYear);
+            }
+
+            if (nextButton.disabled) {
+                nextButton.disabled = false;
+            }
+
+            if (currentYear == new Date().getFullYear() - 2) {
+                prevButton.disabled = true;
+            }
+        });
+
+        const nextButton = document.getElementById("next-year");
+        nextButton.addEventListener("click", () => {
+            if (currentYear < new Date().getFullYear()) {
+                currentYear++;
+                console.log("next year: " + currentYear);
+                renderYear(currentYear);
+            }
+
+            if (prevButton.disabled) {
+                prevButton.disabled = false;
+            }
+
+            if (currentYear == new Date().getFullYear()) {
+                nextButton.disabled = true;
+            }
+        });
+
+        function renderYear(currentYear) {
             const table = document.getElementById("happinessTable");
             const title = document.getElementById("title-str");
-            const currentDate = new Date();
-            const currentYear = currentDate.getFullYear();
+            const summaryDataDiv = document.querySelector('.summary-data');
+            const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+            table.style.visibility = "hidden";
 
             // add year to the title
-            title.textContent += currentYear + " 🤗";
+            title.textContent = "My Happiness in " + currentYear + " 🤗";
 
-            const ratingsCount = new Array(6).fill(0);
+            // clear the table
+            table.innerHTML = "";
+            summaryDataDiv.innerHTML = "";
 
+            initializeTable(table, months);
+
+            chrome.storage.local.get(null, function(items) {
+                const ratingsCount = new Array(6).fill(0);
+
+                // fill the table with ratings from storage
+                Object.keys(items).forEach(item => {
+                    if (item.startsWith("rating_" + currentYear.toString()) && item.length > 7) {
+                        const date = new Date(item.substring(7));
+                        // check if date is valid
+                        if (isNaN(date.getTime())) {
+                            return;
+                        }
+                        const month = date.getMonth();
+                        const day = date.getDate();
+                        const rating = items[item].rating;
+                        const comment = items[item].comment;
+
+                        const emoji = selectEmoji(rating);
+                        const cellColor = selectColor(rating);
+
+                        ratingsCount[rating] += 1;
+
+                        if (table.rows[day] && table.rows[day].cells[month]) {
+                            const cell = table.rows[day + 1].cells[month + 1];
+                            cell.textContent = emoji;
+                            cell.style.backgroundColor = cellColor;
+                            if (comment != null && comment != "") {
+                                cell.title = months[month + 1] + " " + (day + 1) + ": " + comment;
+                            }
+                        }
+                    }
+                });
+
+                // create summary at the top
+                for (let i = 1; i < ratingsCount.length; i++) {
+                    const ratingCount = ratingsCount[i];
+                    const prefixString = getSummaryPrefixString(i.toString());
+                    const dataString = prefixString + ratingCount;
+                    addSummaryData(summaryDataDiv, dataString);
+                }
+
+                table.style.visibility = "visible";
+            });
+        }
+
+        function initializeTable(table, months) {
             const dayRows = 32;
             const monthCols = 13;
 
-            // init table
             for (let i = 0; i < dayRows; i++) {
                 let row = table.insertRow();
                 for (let j = 0; j < monthCols; j++) {
@@ -23,79 +111,19 @@ document.addEventListener(
                 }
             }
 
-            // init header
-            // months
-            const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];  // Months array includes empty string for the first cell
+            // Months header
+
             for (let j = 1; j < monthCols; j++) {
                 table.rows[0].cells[j].textContent = months[j];
-                table.rows[0].cells[j].style.fontWeight = 'bold';
+                table.rows[0].cells[j].style.fontWeight = "bold";
             }
 
-            // days
+            // Days header
             for (let i = 1; i < dayRows; i++) {
                 table.rows[i].cells[0].textContent = i.toString();
-                table.rows[i].cells[0].style.fontWeight = 'bold';
+                table.rows[i].cells[0].style.fontWeight = "bold";
             }
-
-            // fill the table with ratings from storage
-            Object.keys(items).forEach(item => {
-                if (item.startsWith("rating_" + currentYear.toString()) && item.length > 7) {
-                    const date = new Date(item.substring(7));
-                    // check if date is valid
-                    if (isNaN(date.getTime())) {
-                        return;
-                    }
-                    const month = date.getMonth();
-                    const day = date.getDate();
-                    const rating = items[item].rating;
-                    const comment = items[item].comment;
-
-                    const emoji = selectEmoji(rating);
-                    const cellColor = selectColor(rating);
-
-                    ratingsCount[rating] += 1;
-
-                    if (table.rows[day] && table.rows[day].cells[month]) {
-                        const cell = table.rows[day + 1].cells[month + 1];
-                        cell.textContent = emoji;
-                        cell.style.backgroundColor = cellColor;
-                        if (comment != null && comment != "") {
-                            cell.title = months[month + 1] + " " + (day + 1) + ": " + comment;
-                        }
-                    }
-                }
-            });
-
-            // create summary at the top
-            const summaryDataDiv = document.querySelector('.summary-data');
-            for (let i = 1; i < ratingsCount.length; i++) {
-                const ratingCount = ratingsCount[i];
-                const prefixString = getSummaryPrefixString(i.toString());
-                const dataString = prefixString + ratingCount;
-                addSummaryData(summaryDataDiv, dataString);
-            }
-        });
-
-        // function daysToFill(j, currentYear, currentMonth, currentDay) {
-        //     let days = 0;
-        //     // 30 days (Sept, Apr, Jun, Nov)
-        //     if (j == 9 || j == 4 || j == 6 || j == 11) {
-        //         days = 30 + 1; // + 1 because index starts from 1
-        //     } else if (j == 2) {
-        //         // check leap year
-        //         if (currentYear % 4 == 0) {
-        //             // leap year
-        //             days = 29 + 1;
-        //         } else {
-        //             days = 28 + 1;
-        //         }
-        //     } else if (j == currentMonth) {
-        //         days = currentDay;
-        //     } else {
-        //         days = 31 + 1;
-        //     }
-        //     return days;
-        // }
+        }
 
         function addSummaryData(div, content) {
             const summaryElm = document.createElement('h3');
